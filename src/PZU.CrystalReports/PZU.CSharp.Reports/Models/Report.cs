ddf58340567ed7d9734c877c17ecd3e2c7dcfab1
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-12-21 12:53:11 +0100

    Reports

---
+++ src/PZU.CrystalReports/PZU.CSharp.Reports/Models/Report.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZU.CSharp.Reports.Models
+{
+    class Report
+    {
+        public string title;
+        public string filename;
+        public DateTime createDate;
+        public string author;
+
+        // Konstruktor - metoda przeznaczona do konstruowania obiektu wywoływana poprzez new()
+        // Nazwa metody musi być taka sama jak nazwa klasy i bez żadnego parametru wyjściowego
+        // - ustawianie domyślnych wartości
+        // - obowiązkowe pola
+        public Report(string title, string filename, string author = "nieznany")
+        {
+            createDate = DateTime.Now;
+
+            this.title = title;
+            this.filename = filename;
+            this.author = author;
+        }
+
+        //public void Generate()
+        //{
+        //    Console.WriteLine($"{title} {filename} {createDate} {author}");
+        //}
+
+        public void Generate(int copies = 1, int start = 1)    // przeciążanie metod
+        {
+            for (int copy = start; copy <= copies; copy++)
+            {
+                Console.WriteLine($"#{copy} {title} {filename} {createDate} {author}");
+            }
+        }
+
+        public decimal Calculate(int copies, decimal unitPrice)
+        {
+            decimal cost = copies * unitPrice;
+
+            return cost;
+        }
+
+    }
+}
---
+++ src/PZU.CrystalReports/PZU.CSharp.Reports/Program.cs
@@ -0,0 +1,54 @@
+﻿using PZU.CSharp.Reports.Models;
+using System;
+
+namespace PZU.CSharp.Reports
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ReportTest();
+
+            int? x = null;
+
+            string name;
+            DateTime? date;
+
+            if (x.HasValue)
+            {
+                int z = x.Value + 10;
+            }
+
+            int?[] numbers = new int?[10];
+            numbers[0] = 20;
+            numbers[1] = 30;
+            numbers[2] = 10;
+
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+        }
+
+        private static void ReportTest()
+        {
+            Report report1 = new Report("Raport sprzedaży", "raport1.rpt");      // utworzenie instacji obiektu klasy Report
+
+            Console.Write("Podaj ilość kopii: ");
+            int copies = int.Parse(Console.ReadLine());
+
+            report1.Generate(copies, 2);
+
+            report1.Generate();
+
+            decimal cost = report1.Calculate(copies, 0.99m);
+
+            Console.WriteLine($"Koszt wydruku {cost}");
+        }
+    }
+}
