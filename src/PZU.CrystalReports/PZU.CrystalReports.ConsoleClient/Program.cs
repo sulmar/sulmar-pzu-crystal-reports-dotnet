c0584ee01aa33873ec9915ab41c0e5c2f31622f5
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-12-21 14:29:41 +0100

    Reports

---
+++ src/PZU.CrystalReports/PZU.CrystalReports.ConsoleClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZU.CrystalReports.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = System.IO.Path.Combine( System.IO.Directory.GetCurrentDirectory(), "Reports");
+
+            string[] files = System.IO.Directory.GetFiles(path, "*.rpt");
+
+            foreach (string file in files)
+            {
+
+            }
+        }
+    }
+}
