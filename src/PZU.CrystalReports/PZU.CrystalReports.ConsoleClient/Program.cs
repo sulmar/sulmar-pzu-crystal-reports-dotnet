00ab663a8ed1722c0b68da0769df2210be73e5f7
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-12-22 15:03:38 +0100

    SetLocation

--- src/PZU.CrystalReports/PZU.CrystalReports.ConsoleClient/Program.cs
+++ src/PZU.CrystalReports/PZU.CrystalReports.ConsoleClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,58 +25,97 @@ namespace PZU.CrystalReports.ConsoleClient
 
             foreach (string file in files)
             {
-                Console.WriteLine(file);
-
                 ReportDocument report = new ReportDocument();
                 report.Load(file);
 
-                foreach (Table table in report.Database.Tables)
-                {
-                    Console.WriteLine(table.Name);
-                }
-
-                var parameters = report.ParameterFields;
-
-                foreach (CrystalDecisions.Shared.ParameterField parameter in parameters)
-                {
-                    Console.WriteLine($"{parameter.Name} {parameter.ParameterValueType}");
-                }
-
-                var formulaFields = report.DataDefinition.FormulaFields;
-
-                foreach (FormulaFieldDefinition formulaField in formulaFields)
-                {
-                    Console.WriteLine($"{formulaField.Name} {formulaField.Text}");
-                }
-
-                string formulaName = "Version";
-
-                try
-                {
-                    var formula = report.DataDefinition.FormulaFields[formulaName];
-                    formula.Text = "\"SABA 6.0\"";
-                    report.SaveAs(file);
-                }
-                catch(System.Runtime.InteropServices.COMException e)
-                {
-
-                }
-
-                // wyjście - plik CSV
-
-                // | nazwa raportu | nazwa tabeli |  typ
-
-                // | nazwa raportu | nazwa widoku |
-
-                // | nazwa raportu | nazwa procedury | 
-
-                // | nazwa raportu | nazwa sql |
-
-
+                SetLocation(report);
+                GetTables(report);
+                GetParameters(report);
+                GetFormulas(report);
+                ChangeFormula(report);
+                report.SaveAs(file);
             }
 
             Console.WriteLine("Press any key to exit.");
             Console.ReadLine();
+        }
+
+        private static void SetLocation(ReportDocument report)
+        {
+            ConnectionInfo targetConnection = new ConnectionInfo();
+            targetConnection.DatabaseName = "Sakila2";
+
+            foreach (Table table in report.Database.Tables)
+            {
+                ConnectionInfo connectionInfo = table.LogOnInfo.ConnectionInfo;
+
+                table.LogOnInfo.ConnectionInfo = targetConnection;
+
+                table.ApplyLogOnInfo(table.LogOnInfo);
+
+                if (table.TestConnectivity())
+                {
+                   
+                }
+            }
+
+            foreach(ReportObject reportObject in report.ReportDefinition.ReportObjects)
+            {
+                if (reportObject.Kind == ReportObjectKind.SubreportObject)
+                {
+                    SubreportObject subreportObject = (SubreportObject)reportObject;
+
+                    ReportDocument subreport = subreportObject.OpenSubreport(subreportObject.SubreportName);
+
+                    SetLocation(subreport);
+
+                }
+            }
+        }
+
+
+        private static void GetTables(ReportDocument report)
+        {
+            foreach (Table table in report.Database.Tables)
+            {
+                Console.WriteLine(table.Name);
+            }
+        }
+
+        private static void ChangeFormula(ReportDocument report)
+        {
+            string formulaName = "Version";
+
+            try
+            {
+                var formula = report.DataDefinition.FormulaFields[formulaName];
+                formula.Text = "\"SABA 6.0\"";
+          
+            }
+            catch (System.Runtime.InteropServices.COMException e)
+            {
+
+            }
+        }
+
+        private static void GetFormulas(ReportDocument report)
+        {
+            var formulaFields = report.DataDefinition.FormulaFields;
+
+            foreach (FormulaFieldDefinition formulaField in formulaFields)
+            {
+                Console.WriteLine($"{formulaField.Name} {formulaField.Text}");
+            }
+        }
+
+        private static void GetParameters(ReportDocument report)
+        {
+            var parameters = report.ParameterFields;
+
+            foreach (CrystalDecisions.Shared.ParameterField parameter in parameters)
+            {
+                Console.WriteLine($"{parameter.Name} {parameter.ParameterValueType}");
+            }
         }
     }
 }
